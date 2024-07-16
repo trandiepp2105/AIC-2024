b6734e04450eb549b50d635eeb80c9b78184026d
Hồ Khánh Duy <22120076@student.hcmus.edu.vn>
2024-07-16 18:06:10 +0700

    Backend - Thêm search vơi faiss va em băng CLIP L14

---
+++ backend/ai/configs.py
@@ -0,0 +1,12 @@
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+
+VIDEO_FOLDER = r'backend/ai/videos'
+FRAME_FOLDER = r'backend/ai/frames'
+
+INDEX_FAISS = r'backend/ai/faiss_index/index_demo.index'
+DIC_PATH = r'backend/ai/faiss_index/index_dic.json'
+
+THRESHOLD_SIMILARITY = 1.5e-2
+
+INDEX = r'backend/ai/faiss_index/index_demo.index'
