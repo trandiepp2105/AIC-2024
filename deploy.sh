44baee6bcdb842a1d03b6c669e0dbb4c67569987
TraDiep <tranvandiepp2105@gmail.com>
2024-08-08 18:01:15 +0700

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Cấu hình các biến
-SERVER_USER="username"
-SERVER_IP="server_ip"
+SERVER_USER="root"
+SERVER_IP="SERVER_IP"
 REMOTE_PROJECT_PATH="/AIC_PROJECT/"
 SSH_KEY_PATH="$HOME/.ssh/id_rsa"
 SSH_PUB_KEY_PATH="$SSH_KEY_PATH.pub"
-EMAIL_SSH_KEY="example@gmail.com"
-GIT_REPO_URL="https://github.com/mygituser/example.git"
+EMAIL_SSH_KEY="EMAIL_SSH_KEY"
+GIT_REPO_URL="GIT_REPO_URL"
 GIT_BRANCH="main"  # Thay đổi theo nhánh bạn muốn
 
 # Kiểm tra các tham số đầu vào
@@ -17,13 +17,14 @@ if [ -z "$SERVER_USER" ] || [ -z "$SERVER_IP" ]; then
 fi
 
 # Kiểm tra nếu SSH key đã tồn tại
-if [ -f "$SSH_KEY_PATH" ]; thì
+if [ -f "$SSH_KEY_PATH" ]; then
   echo "SSH key đã tồn tại tại $SSH_KEY_PATH"
 else
   # Tạo SSH key
   echo "Tạo SSH key..."
-  ssh-keygen -t rsa -b 4096 "$EMAIL_SSH_KEY" -f "$SSH_KEY_PATH" -N ""
+  ssh-keygen -t rsa -b 4096 -C "$EMAIL_SSH_KEY" -f "$SSH_KEY_PATH" -N ""
 fi
+
 
 # Sao chép SSH public key lên server
 echo "Sao chép SSH public key lên server..."
@@ -33,6 +34,7 @@ ssh-copy-id -i "$SSH_PUB_KEY_PATH" $SERVER_USER@$SERVER_IP
 echo "Kết nối vào server và thực hiện các lệnh deploy..."
 ssh $SERVER_USER@$SERVER_IP -i $SSH_KEY_PATH << EOF
   # Cập nhật hệ thống và cài đặt Docker
+
   echo "Cập nhật hệ thống và cài đặt Docker..."
   sudo apt-get update
 
@@ -46,6 +48,22 @@ ssh $SERVER_USER@$SERVER_IP -i $SSH_KEY_PATH << EOF
   echo "Cài đặt Docker Compose..."
   sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '\"tag_name\": \"\K.*?(?=\")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
+
+  # # cài đặt nvidia container toolkit
+  # sudo apt-get update
+
+  # curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  # && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+  # && sudo apt-get update
+
+  # # Install the NVIDIA Container Toolkit packages:
+  # sudo apt-get install -y nvidia-container-toolkit
+
+  # Configure the container runtime by using the nvidia-ctk command:
+  sudo nvidia-ctk runtime configure --runtime=docker
+
+  # Restart the Docker daemon:
+  sudo systemctl restart docker
 
   # Cấu hình tường lửa
   echo "Cấu hình tường lửa..."
@@ -61,12 +79,15 @@ ssh $SERVER_USER@$SERVER_IP -i $SSH_KEY_PATH << EOF
   git clone -b $GIT_BRANCH $GIT_REPO_URL $REMOTE_PROJECT_PATH
 
   # Di chuyển đến thư mục dự án
-  cd $REMOTE_PROJECT_PATH/AIC-2024/Download_video
+  cd $REMOTE_PROJECT_PATH
+  cd AIC-2024
+  cd Download_video
   pip3 install -r requirements.txt
   python3 download.py
 
   # Khởi động các container Docker
-  cd ../ai
+  cd ..
+  cd ai
   echo "Khởi động các container Docker..."
   docker-compose up -d
   echo "Quá trình deploy hoàn tất!"
